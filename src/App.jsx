ad249ab54feb32865af7bfaa586eb64f13d6aa7b
TaiDoan <doananhtai20@gmail.com>
2025-02-09 14:38:26 +0700

    video 27

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,16 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
-function App() {
+const App = () => {
   const [count, setCount] = useState(0)
+
+  // function myFunction () {
+  //   console.log(">>>>> running")
+  // }
+  const myFunction =() => {
+    console.log("running>>>>>>")
+  }
+  myFunction();
 
   return (
     <>
@@ -16,7 +24,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
+      <h1>taidoan</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 9)}>
           count is {count}
