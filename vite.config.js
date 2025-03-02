7ede8f77a444f5c30439b2e9acfbf2c65f1490c3
TaiDoan <doananhtai20@gmail.com>
2025-03-02 14:29:13 +0700

    note

--- vite.config.js
+++ vite.config.js
@@ -7,6 +7,7 @@ dns.setDefaultResultOrder('verbatim')
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/taidoan-react',
   server: {
     port: 3000,
   },
